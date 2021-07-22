41d94af9110285f6e44c8951b2a51fa6a7c62eda
Patrik Svensson <patrik@patriksvensson.se>
2021-07-22 12:47:15 +0200

    Add CLI examples

--- Table2Ascii/Program.cs
+++ Table2Ascii/Program.cs
@@ -7,6 +7,21 @@ namespace TableToAscii
         public static int Main(string[] args)
         {
             var app = new CommandApp<DefaultCommand>();
+            app.Configure(config =>
+            {
+                config.AddExample(new[] 
+                { 
+                    "input.html", "--output", "output.txt",
+                    "--column", "5", "--column", "10"
+                });
+
+                config.AddExample(new[]
+                {
+                    "input.html", "--output", "output.txt", "--width", "80"
+                });
+
+                config.ValidateExamples();
+            });
             return app.Run(args);
         }
     }
